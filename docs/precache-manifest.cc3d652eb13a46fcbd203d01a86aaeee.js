bc30eb47095819b1965f4820e64eff23a31857b5
Juuso Kosonen <juuson@gmail.com>
2021-03-03 10:28:42 +0000

    deploy the bundle

---
+++ docs/precache-manifest.cc3d652eb13a46fcbd203d01a86aaeee.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63aadaa04e1b55191a4497e88574c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a7f1d308f38b0b2a140",
+    "url": "/static/css/12.849a7a81.chunk.css"
+  },
+  {
+    "revision": "c081f86a0beae026bfc3",
+    "url": "/static/css/6.5de1a8e2.chunk.css"
+  },
+  {
+    "revision": "96f19d343306608fa0ad",
+    "url": "/static/css/8.0e433876.chunk.css"
+  },
+  {
+    "revision": "f39e65c2eb11a40478a7",
+    "url": "/static/css/main.fd30ea3e.chunk.css"
+  },
+  {
+    "revision": "79f9dbde4bffe4f689a3",
+    "url": "/static/js/0.d019d707.chunk.js"
+  },
+  {
+    "revision": "afda71ca3445452e3ce6",
+    "url": "/static/js/1.b1a981fa.chunk.js"
+  },
+  {
+    "revision": "a5e922684d8c8f09e703",
+    "url": "/static/js/10.1a926af0.chunk.js"
+  },
+  {
+    "revision": "f425274883daa0b01156",
+    "url": "/static/js/11.fa97005f.chunk.js"
+  },
+  {
+    "revision": "0a7f1d308f38b0b2a140",
+    "url": "/static/js/12.90842c43.chunk.js"
+  },
+  {
+    "revision": "48763b73d8c195527ebddd5857148be0",
+    "url": "/static/js/12.90842c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702c2de1e56fba886d93",
+    "url": "/static/js/13.ead11b1d.chunk.js"
+  },
+  {
+    "revision": "84ba0dd1cd59851b2418",
+    "url": "/static/js/14.60a6e4e2.chunk.js"
+  },
+  {
+    "revision": "fc5d1e192e18c997d567",
+    "url": "/static/js/15.168b8777.chunk.js"
+  },
+  {
+    "revision": "d6ea24037c7ff64fe38b",
+    "url": "/static/js/16.ed48bbc9.chunk.js"
+  },
+  {
+    "revision": "461ab4fb109c79390687",
+    "url": "/static/js/17.c5217df5.chunk.js"
+  },
+  {
+    "revision": "92a3e82c133708d10ebf",
+    "url": "/static/js/2.44c87cd9.chunk.js"
+  },
+  {
+    "revision": "55a26e2da3ec6ad72764",
+    "url": "/static/js/5.52e0b078.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/5.52e0b078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c081f86a0beae026bfc3",
+    "url": "/static/js/6.76631d1a.chunk.js"
+  },
+  {
+    "revision": "0a7f09c906929fbdc63f3acbd3ddec39",
+    "url": "/static/js/6.76631d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5842a14335aa49bcf101",
+    "url": "/static/js/7.4163df60.chunk.js"
+  },
+  {
+    "revision": "96f19d343306608fa0ad",
+    "url": "/static/js/8.5b9601da.chunk.js"
+  },
+  {
+    "revision": "d97ffad493675aecc188",
+    "url": "/static/js/9.a13918de.chunk.js"
+  },
+  {
+    "revision": "f39e65c2eb11a40478a7",
+    "url": "/static/js/main.8322d6fc.chunk.js"
+  },
+  {
+    "revision": "535276c8923bf7b25586",
+    "url": "/static/js/runtime-main.aa32791c.js"
+  },
+  {
+    "revision": "f1409de6acd8357bd73ca38ab79a616b",
+    "url": "/static/media/Erangel_map.f1409de6.png"
+  },
+  {
+    "revision": "9a19ab583a2c8801d89254650a977753",
+    "url": "/static/media/Miramar_map.9a19ab58.png"
+  },
+  {
+    "revision": "d693454bc00c77e82c5a9c1dd7c6daf8",
+    "url": "/static/media/PUBG-Character.d693454b.png"
+  },
+  {
+    "revision": "2c62510438bf8dd76dc840676958366b",
+    "url": "/static/media/Sanhok_map.2c625104.png"
+  },
+  {
+    "revision": "500324610235faf047e0c065d9feeb2c",
+    "url": "/static/media/Vikendi_map.50032461.png"
+  },
+  {
+    "revision": "0098f5317a6c32ae01ef299d4f34c30a",
+    "url": "/static/media/bg.0098f531.jpg"
+  },
+  {
+    "revision": "037b8e66e917a07cdecd41fade6ce77c",
+    "url": "/static/media/bg.037b8e66.png"
+  },
+  {
+    "revision": "2bbc314936e35e25a8bd88a21e0bf6db",
+    "url": "/static/media/bullseye.2bbc3149.png"
+  },
+  {
+    "revision": "ab8ef3c83aff07fb042be44e2b26beea",
+    "url": "/static/media/care-package.ab8ef3c8.png"
+  },
+  {
+    "revision": "a71bb9c5a71b2465c397b5bc7218015d",
+    "url": "/static/media/character1.a71bb9c5.png"
+  },
+  {
+    "revision": "09ae5f626a2e8952061ab94c9655c9eb",
+    "url": "/static/media/character2.09ae5f62.png"
+  },
+  {
+    "revision": "41cc557bac67a34f471b68237c93f872",
+    "url": "/static/media/character3.41cc557b.png"
+  },
+  {
+    "revision": "b0d67073376f4c8c99c0a01ca5182176",
+    "url": "/static/media/damage.b0d67073.png"
+  },
+  {
+    "revision": "f034978942ab763f8a70793e228f2d3c",
+    "url": "/static/media/heal.f0349789.png"
+  },
+  {
+    "revision": "d127695b8558f31b2470ad12260763f6",
+    "url": "/static/media/kanaliiga-logo.d127695b.png"
+  },
+  {
+    "revision": "40f34c497f00c0e2a7100c2d3c153398",
+    "url": "/static/media/logo.40f34c49.png"
+  },
+  {
+    "revision": "b4e2ddc4499473589c7a0985d4f323b9",
+    "url": "/static/media/logo_hover.b4e2ddc4.png"
+  },
+  {
+    "revision": "2d7373ed35efd18621fc7bd679c906ea",
+    "url": "/static/media/nade.2d7373ed.png"
+  },
+  {
+    "revision": "1d770d4f5ce4ce3426b815e801bc65a2",
+    "url": "/static/media/pubgtextlogo.1d770d4f.png"
+  },
+  {
+    "revision": "b47c55553dbff75f6335ffe9ca5c9c4c",
+    "url": "/static/media/scope.b47c5555.png"
+  },
+  {
+    "revision": "ada7c6ccfdbab3ed92c55a08744890af",
+    "url": "/static/media/steal.ada7c6cc.png"
+  },
+  {
+    "revision": "f266721d13635223c29904c2590d3018",
+    "url": "/static/media/tires.f266721d.png"
+  },
+  {
+    "revision": "def3814f8427ffbfdff0cfc53c4429a3",
+    "url": "/static/media/vehicle.def3814f.png"
+  }
+]);
